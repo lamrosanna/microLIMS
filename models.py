f78f4289e1eefc4ab731fb0d252ecf8f71428bd0
Rosanna Lam <gob@Rosannas-MacBook-Air.local>
2021-09-29 10:19:49 -0700

    adding files

--- models.py
+++
@@ -1,6 +0,0 @@
-
-from django.db import models
-
-# Create your models here.
-class brewery(models.Model):
-    brewery_name = models.CharField(max_length=100)
--- serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-from apitest1.models import brewery
-
-class brewery_serializer(serializers.Serializer):
-    name = serializers.CharField(max_length = 100)
